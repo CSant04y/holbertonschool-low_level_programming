f12a8806260f18c4e252a9a1cb2a5c71924bdd6a
CSant04y <esquivelcarlos27893@gmail.com>
2020-09-17 19:23:57 +0000

    fixed spacing

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
+
 /**
  * main - This prints the entire alphabet except q and e
  * Return: the return is 0
  */
 int main(void)
 {
-        char alph = 'a';
+	char alph = 'a';
 
-        while (alph <= 'z')
-        {
-                if (alph != 'q' && alph != 'e')
-                        putchar(alph);
-                ++alph;
-        }
-        putchar('\n');
-        return (0);
+	while (alph <= 'z')
+	{
+		if (alph != 'q' && alph != 'e')
+			putchar(alph);
+		++alph;
+	}
+	putchar('\n');
+	return (0);
 }
