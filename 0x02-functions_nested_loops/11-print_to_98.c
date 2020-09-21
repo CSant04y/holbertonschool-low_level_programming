75869c14d1e9a5689c5f824575399d30c24b83ac
CSant04y <esquivelcarlos27893@gmail.com>
2020-09-21 21:16:32 +0000

    used the standard library to print of natural numebrs

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * print_to_98 - this prints all the natural number to 98
+ * @n: this is where we are starting from
+ * Return: nothing
+ */
+
+void print_to_98(int n)
+{
+	while (n <= 98)
+	{
+		if (n != 98)
+			printf("%d, ", n);
+		else if (n == 98)
+			printf("%d\n", n);
+		n++;
+	}
+}
