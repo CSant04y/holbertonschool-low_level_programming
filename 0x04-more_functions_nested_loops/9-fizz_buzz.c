ae2f3b8e5f5f557a5ab41ed3db77091c63806c93
CSant04y <esquivelcarlos27893@gmail.com>
2020-09-24 21:10:11 +0000

    added a conditonal statment chnage

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -3,28 +3,21 @@
  * main - this is the fizz buzz
  * Return: 0
  */
-
 int main(void)
 {
 	int i;
-
 	for (i = 1; i <= 100; i++)
 	{
-		if ((i % 3) == 0 && (i % 5) == 0)
-		{
-			printf("FizzBuzz ");
-		}
-		else if ((i % 3) == 0)
-		{
-			printf("Fizz ");
-			continue;
-		}
-		else if ((i % 5) == 0)
-		{
-			printf("Buzz ");
-			continue;
-		}
-		printf("%d ", i);
+		if (i % 3 == 0)
+			printf("Fizz");
+		if (i % 5 == 0)
+			printf("Buzz");
+		if (i % 3 != 0 && i % 5 != 0)
+			printf("%d", i);
+		if (i != 100)
+			printf(" ");
+		if (i == 100)
+			printf("\n");
 	}
 	return (0);
 }
