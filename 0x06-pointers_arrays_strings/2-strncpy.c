0cd3dd4fffbbcdba8c73ff9496b8ed71ed5274ba
CSant04y <esquivelcarlos27893@gmail.com>
2020-10-01 16:19:33 +0000

    used two for loops

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ * *stncpy - this function copies a string
+ * @dest: this is the dest string
+ * @src: this is the source string
+ * @n: this is up to n bytes
+ * Return: the string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int cycle;
+
+	for (cycle = 0; cycle < n && src[cycle] != '\0'; cycle++)
+	{
+		dest[cycle] = src[cycle];
+	}
+	for (; cycle < n; cycle++)
+	{
+		dest[cycle] = '\0';
+	}
+	return (dest);
+}
