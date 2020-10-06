1fa451252cd9866f7aa587000ea4dc7814c4d38d
CSant04y <esquivelcarlos27893@gmail.com>
2020-10-06 03:30:34 +0000

    used a nested loop

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * print_chessborad - this print the chess board
+ * @a: this is a pointer to an array
+ *
+ */
+void print_chessboard(char (*a)[8])
+{
+	int j, i;
+
+	for (j = 0; j < 8; j++)
+	{
+		for (i = 0; i < 8; i++)
+		{
+			_putchar(a[j][i]);
+		}
+		_putchar('\n');
+	}
+}
