60048c14c0e139e44032e99bb2df1b3265aa452c
CSant04y <esquivelcarlos27893@gmail.com>
2020-10-06 02:17:03 +0000

    got my code to work

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ * *_strstr - write a function that locates a substring
+ * @haystack: this is string
+ * @needle: this is a substring
+ * Return: a char
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i, itr;
+
+	for (i = 0; haystack[i]; i++)
+	{
+		for (itr = 0; needle[itr]; itr++)
+		{
+			if (haystack[i + 1] == *needle)
+				return (&haystack[i + 1]);
+		}
+	}
+
+	return (haystack);
+}
