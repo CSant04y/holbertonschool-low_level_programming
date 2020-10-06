04f2aad8c346b106c94d2f8a41cf499e57d6fd73
CSant04y <esquivelcarlos27893@gmail.com>
2020-10-06 03:46:54 +0000

    swaped variables

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+ * set_string - this sets the value of a pointer to a char
+ * @s: this is a double pointer
+ * @to: this is setting a string
+ * Return: void
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
