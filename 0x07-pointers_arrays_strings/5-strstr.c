22d7b8ae8a710fc27de95da99f9df3f2601418f1
CSant04y <esquivelcarlos27893@gmail.com>
2020-10-06 02:37:25 +0000

    reverted back

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -17,6 +17,8 @@ char *_strstr(char *haystack, char *needle)
 			if (haystack[i + 1] == *needle)
 				return (&haystack[i + 1]);
 		}
+		if (haystack[i + 1] == 0)
+			return (&haystack[i + 1]);
 	}
 	return (NULL);
 }
