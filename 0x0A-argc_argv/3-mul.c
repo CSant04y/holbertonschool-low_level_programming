8a3e2324676dfba1b65086fd51fa6c9d24946988
CSant04y <esquivelcarlos27893@gmail.com>
2020-10-11 15:38:49 +0000

    added betty comments

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - this multiplies two ints
+ * @argc: this is the args in the command line
+ * @argv: this is the string vector
+ * Return: 0, and reult
+ */
+
+int main(int argc, char **argv)
+{
+        int mul;
+
+        if (argc != 3)
+        {
+		printf("Error\n");
+		return (1);
+        }
+        else
+        {
+                mul = atoi(argv[1]) * atoi(argv[2]);
+                printf("%d\n", mul);
+        }
+        return (0);
+}
