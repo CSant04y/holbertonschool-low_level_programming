86497743cc288a10db48d13c0be5890e54b79ad5
CSant04y <esquivelcarlos27893@gmail.com>
2020-10-13 18:17:45 +0000

    chnaged the code lay out

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,23 +1,28 @@
 #include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
 /**
- * *_strdup - this duplicates a string and allocated space and memeory for it
- * @str: pointer to the string that is passed
- * Return: is our char pointer
+ * *_strdup - this copies a string and allocated memory for it
+ * @str: this is the string we want to copy
+ * Return: return is a char
  */
+
 char *_strdup(char *str)
 {
 	char *ptr;
-	unsigned int count = 0;
+	int count = 0;
 
-	if (*str == NULL)
+	if (str == NULL)
 		return (NULL);
 	while (str[count] != '\0')
 		count++;
 	ptr = malloc(sizeof(char) * (count + 1));
 	if (ptr == NULL)
 		return (NULL);
-	for (count = 0; str[count]; count++)
+	for (count = 0; str[count] != '\0'; count++)
+	{
 		ptr[count] = str[count];
+	}
 	ptr[count] = '\0';
 	return (ptr);
 }
