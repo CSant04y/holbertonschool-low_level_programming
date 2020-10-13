bbbe5c698bbf3f12b9ab86f290780890581b4330
CSant04y <esquivelcarlos27893@gmail.com>
2020-10-13 19:49:21 +0000

    added bulk files

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,39 @@
+#include "holberton.h"
+/**
+ * **alloc_grid - this returns a to a two demansional array of char
+ * @width: this it the width of grid
+ * @height: this it the height of the grid
+ * Return: int pointer
+ */
+int **alloc_grid(int width, int height)
+{
+	int **ptr, h, w;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+	ptr = malloc(sizeof(int *) * height);
+	if(!ptr)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	for (h = 0; h < height; h++)
+	{
+		ptr[h] = malloc(sizeof(int) * width);
+		if (ptr[h] == NULL)
+		{
+			while (h >= 0)
+			{
+				free(ptr[h]);
+				h--;
+			}
+			free(ptr);
+			return (NULL);
+		}
+		for(w = 0; w < width; w++)
+		{
+			ptr[h][w] = 0;
+		}
+	}
+	return (ptr);
+}
