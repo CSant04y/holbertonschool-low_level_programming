20d5cbccb88cd89c553a1701d63870f8da448003
CSant04y <esquivelcarlos27893@gmail.com>
2020-10-13 18:22:25 +0000

    added some changes to the code

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,40 +1,36 @@
 #include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
 /**
- * *str_concat - this concatenates two strings
- * @s1: string 1
+ * *str_concat - this concatanates another string to another
+ * @s1: String 1
  * @s2: string 2
- * Return: char pointer
+ * Return: return is a char
  */
+
 char *str_concat(char *s1, char *s2)
 {
 	char *ptr;
-	unsigned int len, i, j;
+	int count2 = 0, count1 = 0, i, j;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	len = (_strlenr(s1) + _strlenr(s2));
-	ptr = malloc(sizeof(char) * len);
+	while (s1[count1] != '\0')
+	{
+		count1++;
+	}
+	while (s2[count2] != '\0')
+	{
+		count2++;
+	}
+	ptr = malloc(sizeof(char) * (count1 + count2 + 1));
 	if (ptr == NULL)
 		return (NULL);
-	for (i = 0; s1[i]; i++)
+	for (i = 0; i <= count1 - 1; i++)
 		ptr[i] = s1[i];
-	for (j = 0; s2[j]; j++, i++)
-		ptr[i] = s2[j];
-	ptr[i] = '\0';
+	for (j = 0; j <= count2; j++)
+		ptr[j + i] = s2[j];
 	return (ptr);
-}
-
-/**
- * _strlenr - this find the length of a string recursevly
- * @s: this is a pointer to the beggining of the string in c
- * Return: size of a string
- */
-int _strlenr(char *s)
-{
-	if (*s == '\0')
-		return (0);
-	else
-		return (1 + _strlenr(s + 1));
 }
