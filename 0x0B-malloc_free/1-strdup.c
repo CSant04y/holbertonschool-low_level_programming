949a1e2462c012ab98d710c06bbe84595bb39676
CSant04y <esquivelcarlos27893@gmail.com>
2020-10-13 15:35:51 +0000

    duplicated a string after mallocing space for it

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ * *_strdup - this duplicates a string and allocated space and memeory for it
+ * @str: pointer to the string that is passed
+ * Return: is our char pointer
+ */
+char *_strdup(char *str)
+{
+	char *ptr;
+	unsigned int count = 0;
+
+	if (*str == 0)
+		return (NULL);
+	while (str[count] != '\0')
+		count++;
+	ptr = malloc(sizeof(char) * count);
+	if (ptr == NULL)
+		return (NULL);
+	for (count = 0; str[count]; count++)
+		ptr[count] = str[count];
+	ptr[count] = '\0';
+	return (ptr);
+}
