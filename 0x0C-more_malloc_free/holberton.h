495e98f0e65833c670bdd3a216a971a981e30cea
CSant04y <esquivelcarlos27893@gmail.com>
2020-10-15 17:38:34 +0000

    added headerfile changes

--- 0x0C-more_malloc_free/holberton.h
+++ 0x0C-more_malloc_free/holberton.h
@@ -1,5 +1,7 @@
-#include _HOLBERTON_H_
+#ifndef _HOLBERTON_H_
 #define _HOLBERTON_H_
+#include <stdlib.h>
+#include <stdio.h>
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
