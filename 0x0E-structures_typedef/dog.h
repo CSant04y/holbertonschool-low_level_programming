f1fbe1c0588172cc09d3e1df8223ca799a87c089
CSant04y <esquivelcarlos27893@gmail.com>
2020-10-19 16:18:53 +0000

    added files for test

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef _DOG_
+#define _DOG_
+/**
+ * struct dog - this is the name of the struct
+ * @name: this is the name of the type
+ * @age: is a float of age
+ * @owner: this is the owner
+ * This is the first stuct of this project
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
