62676ea88ea30ea578210e84d543c34a80debe62
CSant04y <esquivelcarlos27893@gmail.com>
2020-10-19 15:04:28 +0000

    added file for test

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+ * main - this is the main file that prints the file it is from
+ *
+ * Return: is file name it was compiled
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
