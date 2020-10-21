36716fd538b002190802e65778dcd64581a85c5a
CSant04y <esquivelcarlos27893@gmail.com>
2020-10-21 15:34:36 +0000

    added repo set up

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,8 +1,15 @@
 #include "function_pointers.h"
 /**
- *
- *
- *
- *
+ * print_name - this passes a string to this function and we return to func *
+ * @name: is the string is being passed to us
+ * @f: the is a function pointer
+ * Return: nothing
  */
-void print_name(char *name, void (*f)(char *));
+void print_name(char *name, void (*f)(char *))
+{
+	if (!name)
+		return;
+	if (!f)
+		return;
+	f(name);
+}
