95e4fd6007fcee249cd08bbc473e1d23852cfc92
CSant04y <esquivelcarlos27893@gmail.com>
2020-11-04 19:38:35 +0000

    getting correct node

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * *get_nodeint_at_index - This gets the nth node in a list and returns it
+ * @head: this is a pointer to the head of the list
+ * @index: this is the index of the list
+ * Return: is head
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int idx = 0;
+
+	while (idx < index)
+	{
+		head = head->next;
+		idx++;
+	}
+	return (head);
+}
