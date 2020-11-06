7a1a0074ae0783e4d7708d2895835c75097ea5c3
CSant04y <esquivelcarlos27893@gmail.com>
2020-11-06 02:57:58 +0000

    added betty fixes

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -3,11 +3,11 @@
  * flip_bits - this flips the bits
  * @n: int n
  * @m: m
- * return: 0
+ * Return: 0
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	(void *)n;
-	(void *)m;
+	(void) n;
+	(void) m;
 	return (0);
 }
