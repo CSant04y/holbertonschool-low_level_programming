31eecbc50447fdf9bf5c8d77e41ddc72eca05bf8
CSant04y <esquivelcarlos27893@gmail.com>
2020-11-07 02:33:44 +0000

    adding file to test

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -7,8 +7,12 @@
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	if (index > 64)
+	unsigned long int var;
+
+	if (index >= (sizeof(unsigned long int) * 8))
 		return (-1);
-	*n = (1 >> index) | *n;
+	var = ~(1 << index);
+
+	*n = *n & var;
 	return (1);
 }
