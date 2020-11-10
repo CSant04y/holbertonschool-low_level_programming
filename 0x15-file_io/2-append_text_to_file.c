e5bddbe8505ccf03dfc1682ed2b964d0c76df778
CSant04y <esquivelcarlos27893@gmail.com>
2020-11-10 01:45:38 +0000

    testing placemnt of loop

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -14,13 +14,14 @@ int append_text_to_file(const char *filename, char *text_content)
 	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd == -1 || filename == NULL)
 		return (-1);
-	while (text_content[count])
-		count++;
+
 	if (text_content == NULL)
 	{
 		close(fd);
 		return (1);
 	}
+	while (text_content[count])
+		count++;
 	wrt = write(fd, text_content, count);
 	if (wrt == -1)
 	{
