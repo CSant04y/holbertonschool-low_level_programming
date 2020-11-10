9d66d30781c0c47cd789a9b7026f372cb39c11f2
CSant04y <esquivelcarlos27893@gmail.com>
2020-11-10 01:51:07 +0000

    adding to test

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,12 @@
+/**
+ * main - This copies one file into another
+ * @ac: argument count
+ * @av: argument vector
+ * Return: Always 0.
+ */
+int main(int ac, char **av)
+{
+	(void) ac;
+	(void) av;
+	return (0);
+}
