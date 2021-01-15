be411eef4ac141fe9d74851afe4b119abe537362
CSant04y <esquivelcarlos27893@gmail.com>
2021-01-15 17:19:50 +0000

    added file with betty fixes

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -2,21 +2,19 @@
 /**
  * hash_table_create - This Function creates a Hash table.
  * @size: This is the size of the array that we are passed
- * @description: This function Initilizes the 
- * Return: Returning the pointer hashTable
+ * Return: Returning the pointer to the hashtable array
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-        hash_table_t *hashTable = NULL;
+	hash_table_t *hashTable = NULL;
 
-        hashTable = malloc(sizeof(hash_table_t));
-        if(hashTable == NULL)
-                return (NULL);
+	hashTable = malloc(sizeof(hash_table_t));
+	if (hashTable == NULL)
+		return (NULL);
 
-        hashTable->size = size;
-        hashTable->array = calloc(size, sizeof(hash_node_t *));
-        if (!hashTable->array)
-                return (NULL);
-
-        return (hashTable);
+	hashTable->size = size;
+	hashTable->array = calloc(size, sizeof(hash_node_t *));
+	if (!hashTable->array)
+		return (NULL);
+	return (hashTable);
 }
