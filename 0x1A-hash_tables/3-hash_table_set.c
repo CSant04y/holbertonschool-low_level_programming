46e62b0ab7209fce3162965ed503739dd08c57b8
CSant04y <esquivelcarlos27893@gmail.com>
2021-01-16 01:11:12 +0000

    added files with what hopefult works

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,83 @@
+#include "hash_tables.h"
+/**
+ * hash_table_set - This Function adds element to list
+ * @ht: the hash table you want to add or update the key/value
+ * @key:is the key
+ * @value: is value associated with the key
+ * Return: 1 for success 0 for otherwise.
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	hash_node_t *new_node = NULL;
+	char *key_dup = NULL, *value_dup = NULL;
+	unsigned long int idx_value = 0;
+
+	if (ht == NULL || key == NULL || value == NULL || strcmp(key, "") == 0)
+		return (0);
+
+	key_dup = strdup(key);
+	value_dup = strdup(value);
+	if (key_dup == NULL || value_dup == NULL)
+	{
+		free_func(&new_node, &key_dup, &value_dup);
+		return (0);
+	}
+	new_node = init_node(key_dup, value_dup);
+	if (new_node == NULL)
+	{
+		free_func(&new_node, &key_dup, &value_dup);
+		return (0);
+	}
+	idx_value = key_index((unsigned char *)key_dup, ht->size);
+	if (idx_value == 0)
+	{
+		free_func(&new_node, &key_dup, &value_dup);
+		return (0);
+	}
+	for (; ht->array[idx_value] != NULL; idx_value++)
+	{
+		if (!(strcmp(new_node->key, ht->array[idx_value]->key)))
+		{
+			free(ht->array[idx_value]->value);
+			ht->array[idx_value]->value = value_dup;
+			free_func(&new_node, &key_dup, &value_dup);
+			return (1);
+		}
+	}
+	ht->array[idx_value] = new_node;
+	return (1);
+}
+/**
+ * init_node - This function initilizes the node with a key and value in LL
+ * @key_dup: Duplicate of Key
+ * @value_dup: Duplicate of Value
+ * Return: The new initilized node back to the main function
+ */
+hash_node_t *init_node(char *key_dup, char *value_dup)
+{
+	hash_node_t *new_node = NULL;
+
+	new_node = malloc(sizeof(hash_node_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->key = key_dup;
+	new_node->value = value_dup;
+	new_node->next = NULL;
+	return (new_node);
+}
+/**
+ * free_func - Frees stringss that are duplicates
+ * @new_node: pointer to new node created in the linked list
+ * @key: pointer to duplicated key string
+ * @value: pointer to duplicated value string
+ * freed before exit failure or if value being replaced
+ * Return: Nothing
+ */
+
+void free_func(hash_node_t **new_node, char **key, char **value)
+{
+	free(*new_node);
+	free(*key);
+	free(*value);
+}
