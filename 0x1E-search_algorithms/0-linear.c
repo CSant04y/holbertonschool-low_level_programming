8e6af1bc3df01a2737c5ee4eedd9401b7e1321b9
CSant04y <esquivelcarlos27893@gmail.com>
2021-04-19 18:07:28 +0000

    implemented iterative approach to task

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,25 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - This search algo iterates linearly through an array
+ * @array: This is a pointer to the first element of an array
+ * @size: This is the size of the array
+ * @value: This is the value that is to be searched in an array
+ * Return: the index of the the first occurence of value in the array
+ */
+
+int linear_search(int *array, size_t size, int value)
+{
+	unsigned int itr;
+
+	if (array == NULL)
+		return (-1);
+	for (itr = 0; itr <= size; itr++)
+	{
+		printf("Value checked array[%d] = [%d]\n", itr, array[itr]);
+		if (array[itr] == value)
+			return (itr);
+	}
+	/*Returns negative one as defualt if no value is found*/
+	return (-1);
+}
